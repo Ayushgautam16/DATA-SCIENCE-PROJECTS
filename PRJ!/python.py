2d09ef240c7bc11a5e7ddfffbf7bc3550f0c172d
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-10 00:09:06 +0530

    21

--- PRJ!/python.py
+++ PRJ!/python.py
@@ -1,4 +1,4 @@
-mylist = ["a", "b", "c", "d", "e"]
+# mylist = ["a", "b", "c", "d", "e"]
 # print(mylist)
 
 
@@ -17,3 +17,34 @@ mylist = ["a", "b", "c", "d", "e"]
 # print(mylist)
 
 # mylist.append("f")
+
+
+# mylist1 = [1,-2,4,9,4,687]
+# mylist1.sort()
+# print(mylist1)
+
+
+# mylist2 = [10,20,30,40]
+
+# mylist1.extend(mylist2)
+
+# print(mylist1)
+
+# mylist1.insert(2,50)
+
+# print(mylist1)
+
+# mylist1.remove(4)
+
+# print(mylist1)
+
+# [-2, 1, 4, 4, 9, 687]
+# [-2, 1, 4, 4, 9, 687, 10, 20, 30, 40]
+# [-2, 1, 50, 4, 4, 9, 687, 10, 20, 30, 40]
+# [-2, 1, 50, 4, 9, 687, 10, 20, 30, 40]
+
+# mylist = [10,20,30,40]
+# a= mylist[1:3]
+# print(a)
+
+
