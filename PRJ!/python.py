22c02ac6f3dace8254a762df7e2331622ea2da29
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-10 00:01:58 +0530

    q

--- PRJ!/python.py
+++ PRJ!/python.py
@@ -1,8 +1,19 @@
 mylist = ["a", "b", "c", "d", "e"]
-print(mylist)
+# print(mylist)
 
 
-if "a" in mylist:
-    mylist.remove("a")
+# if "a" in mylist:
+#     mylist.remove("a")
 
-    print(mylist)
+#     print(mylist)
+
+# if "a" in mylist:
+#     print("aaye aaye captain")
+# else:
+#     print("aaye aaye nahi hai captain")
+
+# mylist.pop(0)
+
+# print(mylist)
+
+# mylist.append("f")
