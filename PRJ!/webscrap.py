ebac33be1456ed1299d9b62d38ae74bf0f575e84
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-11 22:19:51 +0530

    1

---
+++ PRJ!/webscrap.py
@@ -0,0 +1,16 @@
+import os
+import requests
+
+def fetchAndSaveToFile(url, path):
+    # Create directory if it doesn't exist
+    directory = os.path.dirname(path)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    
+    # Fetch the URL content and save to file
+    r = requests.get(url)
+    with open(path, 'w') as f:
+        f.write(r.text)
+
+url = 'https://timesofindia.indiatimes.com/'
+fetchAndSaveToFile(url, "data/times.html")
