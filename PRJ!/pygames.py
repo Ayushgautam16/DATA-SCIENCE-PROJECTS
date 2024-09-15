216414215c1253ff37dd243cd03ead2276a2f237
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-15 23:44:35 +0530

    4

---
+++ PRJ!/pygames.py
@@ -0,0 +1,22 @@
+import turtle
+
+wn = turtle.Screen()
+wn.title("PONG by Ayush")
+wn.bgcolor("black")
+wn.setup(width = 800, height =600)
+wn.tracer(0)
+
+
+# paddle A
+
+paddle_a = turtle.Turtle()
+paddle_a.speed(0)
+paddle_a.shape("square")
+paddle_a.color("white")
+
+paddle_a.background
+
+
+# main game loop
+while True:
+    wn.update()
