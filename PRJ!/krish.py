8f88aaa454e6b64767057a6f5e33093eb302d0e7
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-20 01:12:29 +0530

    1

--- PRJ!/krish.py
+++ PRJ!/krish.py
@@ -11,17 +11,17 @@ print(b)
 
 # For a 3D array (cube)
 
-c = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
-print(c)
+# c = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+# print(c)
 
 
-# get the dimension
-print(a.ndim)
-print(b.ndim)
-print(c.ndim)
-print(a.shape)
-print(b.shape)
-print(c.shape)
+# # get the dimension
+# print(a.ndim)
+# print(b.ndim)
+# print(c.ndim)
+# print(a.shape)
+# print(b.shape)
+# print(c.shape)
 
 # get type?
 
