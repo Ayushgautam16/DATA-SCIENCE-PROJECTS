ed8a0ea7d56eb6f12f4e62f39e7563015ece2ffe
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-21 19:55:04 +0530

    1

--- PRJ!/learnpanda.py
+++ PRJ!/learnpanda.py
@@ -1,9 +1,9 @@
-import pandas as pd 
+# import pandas as pd 
 
-df = pd.DataFrame([[1, 2, 3], [4,5,6],[7,8,9]], columns=["a", "b", "c"])
-df.head()
+# df = pd.DataFrame([[1, 2, 3], [4,5,6],[7,8,9]], columns=["a", "b", "c"])
+# df.head()
 
-print(df.to_csv())
-df.describe()
+# print(df.to_csv())
+# df.describe()
 
-df.sort_values(by="a", ascending=False)
+# df.sort_values(by="a", ascending=False)
