89a843833ad80bdee4a1451afbff992f35522b22
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-22 20:12:18 +0530

    1

--- PRJ!/num.py
+++ PRJ!/num.py
@@ -1,34 +1,34 @@
-# all zero matrix in numpy
-[1, 2, 3, 4, 5]
-print()
-import numpy as np
+# # all zero matrix in numpy
+# [1, 2, 3, 4, 5]
+# print()
+# import numpy as np
 
-matrix = np.zeros((5, 5))
+# matrix = np.zeros((5, 5))
 
-print(matrix)
+# print(matrix)
 
-# all 1s matrix in numpy
+# # all 1s matrix in numpy
 
-matrix_ones = np.ones((5, 5))
+# matrix_ones = np.ones((5, 5))
 
-print(matrix_ones)
-
-
-matrix_ones = np.ones((2, 4,2))
-
-print(matrix_ones)
+# print(matrix_ones)
 
 
-# random num matrix in numpy
+# matrix_ones = np.ones((2, 4,2))
 
-matrix_random = np.random.rand(5, 2)
-
-print(matrix_random)
+# print(matrix_ones)
 
 
+# # random num matrix in numpy
 
-# repeat n times of numpy iteration
+# matrix_random = np.random.rand(5, 2)
 
-arr = np.array([1, 2, 3, 4, 5])
-r1 = np.repeat(arr,3, 0)
-print(r1)
+# print(matrix_random)
+
+
+
+# # repeat n times of numpy iteration
+
+# arr = np.array([1, 2, 3, 4, 5])
+# r1 = np.repeat(arr,3, 0)
+# print(r1)
