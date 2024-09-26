09ea21d3aa722030fd56cbc3663324e45b4fa249
Ayush Gautam <ayushgautam2007.kg@gmail.com>
2024-09-26 15:55:14 +0530

    1

---
+++ PRJ!/project1.py
@@ -0,0 +1,29 @@
+#madlibs using sstring concatination
+
+# youtuber ="ayush gautam"
+
+# print ("subscribe to "+ youtuber)
+# print ("subscribe to {}",format(youtuber))
+# print(f"subscribe to {youtuber}")
+
+
+
+adjective = input("adjective:")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+famous_person = input("famous person: ")
+ 
+madlib = "computer is the mischeviewer in this world{adjective} it makes me so excited all the time because i love to {verb1} stay hydrated and {verb2} like you are" 
+
+print(madlib)
+
+
+# guess the number
+import random
+def guess(x):
+    random_nummber =  random.randint(1,x)
+
+    while guess != random_number:
+        guess = input ("guess a number:"):
+
+        
